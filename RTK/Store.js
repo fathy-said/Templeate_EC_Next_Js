3d3fb4368af4fd82215b1a2fb7f1ceb688664884
saidfathy <fathy50515253@gmail.com>
2023-05-13 23:18:09 +0300

    change RTK folder

--- RTK/Store.js
+++ RTK/Store.js
@@ -1,15 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ProductReducer from "./Reducer/ProductReducer";
 import SearchReducer from "./Reducer/SearchReducer";
-import CategoryReducer from "./Reducer/CategoryReducer";
-import DetailsReducer from "./Reducer/DetailsReducer";
 import CartReducer from "./Reducer/CartReducer";
 export let Store = configureStore({
   reducer: {
-    ProductReducer,
     SearchReducer,
-    CategoryReducer,
-    DetailsReducer,
+
     CartReducer,
   }
 })
